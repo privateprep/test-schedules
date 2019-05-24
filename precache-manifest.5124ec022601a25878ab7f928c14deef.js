e0e01fa57801512379a77527363c873541468492
Dan <danielpowell12@msn.com>
2019-05-24 09:35:14 -0600

    Updates

---
+++ precache-manifest.5124ec022601a25878ab7f928c14deef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bc0ae3ad5bc11da82bf906b7f043798",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3503d3da1fd17526f4e",
+    "url": "/static/css/main.f8c2781b.chunk.css"
+  },
+  {
+    "revision": "2a15fbad5eb8fb6922b9",
+    "url": "/static/js/2.392540ad.chunk.js"
+  },
+  {
+    "revision": "573e7afd388d9cfa8dc4",
+    "url": "/static/js/3.93d2e318.chunk.js"
+  },
+  {
+    "revision": "b3503d3da1fd17526f4e",
+    "url": "/static/js/main.355e0aa8.chunk.js"
+  },
+  {
+    "revision": "d87e915d07ab00c9b8bc",
+    "url": "/static/js/runtime~main.4a79294a.js"
+  },
+  {
+    "revision": "5aab7908fb3a00da9e713ea0c27197a6",
+    "url": "/static/media/logo.5aab7908.svg"
+  },
+  {
+    "revision": "c28fa33385add4be394f53bab837c77c",
+    "url": "/static/media/pin.c28fa333.svg"
+  }
+]);
