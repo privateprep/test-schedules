d564941345a8370dfb560d84958fe365ad0544bd
Dan <danielpowell12@msn.com>
2019-05-23 23:22:30 -0600

    Applies more PP branding

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import React, { Suspense, lazy } from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Link } from "react-router-dom";
 import "./App.css";
 import { ReactComponent as Logo } from "./logo.svg";
+import { ReactComponent as Pin } from "./pin.svg";
+import { locations } from "./lib";
 
 const Location = lazy(() => import("./Location"));
 
+const Home = _ => (
+  <main className="Home">
+    <h1>Select Location</h1>
+    <ul className="locations__list">
+      {locations.map(location => (
+        <li className="locations__list__item">
+          <Link to={`/locations/${location.slug}`} className="location-link">
+            <Pin className="pin" />
+            <h3>{location.name}</h3>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </main>
+);
+
 const Menu = _ => (
   <header className="Menu">
-    <Logo className="Logo" />
-    <nav />
+    <Link to="/">
+      <Logo className="Logo" />
+    </Link>
+    <h1 className="title">Test Schedule</h1>
   </header>
 );
 
 const App = _ => (
   <div className="App">
     <Menu />
-    <Suspense fallback={<p>Loading...</p>}>
-      <Switch>
-        <Route exact path="/locations/:slug" component={Location} />
-      </Switch>
-    </Suspense>
+    <div className="App-body">
+      <Suspense fallback={<p>Loading...</p>}>
+        <Switch>
+          <Route exact path="/locations/:slug" component={Location} />
+          <Route component={Home} />
+        </Switch>
+      </Suspense>
+    </div>
   </div>
 );
 
--- src/Location.js
+++ src/Location.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Menu } from "./App";
 import { findLocationBySlug, fetchLocationTests } from "./lib";
 
 const Location = ({
@@ -44,82 +45,88 @@ const Location = ({
   }
 
   return (
-    <div className="Location">
-      <header className="Location__header">
-        <h1 className="title">{location.name} Test Schedule</h1>
-      </header>
-      {isLoading && <p>Loading...</p>}
-      {!isLoading && !!testTypes.length && (
-        <React.Fragment>
-          <div class="filters">
-            <h4>Test Types</h4>
-            <div className="radio-group">
-              <div className="radio" onClick={_ => setActiveTestType(null)}>
-                <label className="radio-label">
-                  <input type="radio" value={null} checked={!activeTestType} />
-                  <span className="option-label">View All</span>
-                </label>
-              </div>
-              {testTypes.map(testType => (
-                <div
-                  className="radio"
-                  key={testType.test_type_id}
-                  onClick={_ => setActiveTestType(testType.test_type)}
-                >
+    <React.Fragment>
+      <div className="Location">
+        <header className="Location__header">
+          <h1 className="title">{location.name} Test Schedule</h1>
+        </header>
+        {isLoading && <p>Loading...</p>}
+        {!isLoading && !!testTypes.length && (
+          <React.Fragment>
+            <div class="filters">
+              <h2>Test Types</h2>
+              <div className="radio-group">
+                <div className="radio" onClick={_ => setActiveTestType(null)}>
                   <label className="radio-label">
                     <input
                       type="radio"
-                      value={testType.test_type}
-                      checked={activeTestType === testType.test_type}
+                      value={null}
+                      checked={!activeTestType}
                     />
-                    <span className="option-label">{testType.test_type}</span>
+                    <span className="option-label">All</span>
                   </label>
                 </div>
-              ))}
+                {testTypes.map(testType => (
+                  <div
+                    className="radio"
+                    key={testType.test_type_id}
+                    onClick={_ => setActiveTestType(testType.test_type)}
+                  >
+                    <label className="radio-label">
+                      <input
+                        type="radio"
+                        value={testType.test_type}
+                        checked={activeTestType === testType.test_type}
+                      />
+                      <span className="option-label">{testType.test_type}</span>
+                    </label>
+                  </div>
+                ))}
+              </div>
             </div>
+          </React.Fragment>
+        )}
+        {!isLoading && !!activeTests.length && (
+          <div class="test-container">
+            <header class="test-container__header">
+              <h2>Available Tests</h2>
+              <p class="label">{activeTests.length}</p>
+            </header>
+            <table>
+              <thead>
+                <tr>
+                  <th className="testType">Test Type</th>
+                  <th className="testCenter">Test Center</th>
+                  <th className="startsAt">Start Time</th>
+                  <th className="accommodations">Accommodations</th>
+                </tr>
+              </thead>
+              <tbody>
+                {activeTests.map(test => {
+                  const localDate = new Date(test.starts_at);
+                  return (
+                    <tr key={`${test.id}-${test.test_type}`}>
+                      <td className="testType">{test.test_type}</td>
+                      <td className="testCenter">{test.test_center}</td>
+                      <td className="startsAt">
+                        {localDate.toLocaleString("default", {
+                          timeZoneName: "short",
+                        })}
+                      </td>
+                      <td className="accommodations">
+                        {test.accommodations.map(accom => (
+                          <p>{accom.name}</p>
+                        ))}
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
           </div>
-        </React.Fragment>
-      )}
-      {!isLoading && !!activeTests.length && (
-        <div class="test-container">
-          <header class="test-container__header">
-            <h2>Available Tests</h2>
-            <p class="label">{activeTests.length}</p>
-          </header>
-          <table>
-            <thead>
-              <tr>
-                <th className="testType">Test Type</th>
-                <th className="testCenter">Test Center</th>
-                <th className="startsAt">Start Time</th>
-                <th className="accommodations">Accommodations</th>
-              </tr>
-            </thead>
-            <tbody>
-              {activeTests.map(test => {
-                const localDate = new Date(test.starts_at);
-                return (
-                  <tr key={`${test.id}-${test.test_type}`}>
-                    <td className="testType">{test.test_type}</td>
-                    <td className="testCenter">{test.test_center}</td>
-                    <td className="startsAt">
-                      {localDate.toLocaleString("default", {
-                        timeZoneName: "short",
-                      })}
-                    </td>
-                    <td className="accommodations">
-                      {test.accommodations.map(accom => (
-                        <p>{accom.name}</p>
-                      ))}
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+    </React.Fragment>
   );
 };
 
